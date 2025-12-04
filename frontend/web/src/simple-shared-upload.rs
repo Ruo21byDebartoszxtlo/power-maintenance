4d91cf51ae6e3dd7ea79e4dadb30a5bf5c733eb0
Ruo21byDebartoszxtlo <i-litvinov907088157@rambler.ru>
2025-12-04 10:46:13 +0800

    add package.json, upload-product.py, simple-shared-upload.rs

---
+++ frontend/web/src/simple-shared-upload.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\simple-shared-upload.rs
+// Generated at: 2025-10-07T11:19:19.361Z
---
+++ types-product/layouts-spec/upload-product.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\types-product\layouts-spec\upload-product.py
+// Generated at: 2025-10-07T11:19:19.352Z
